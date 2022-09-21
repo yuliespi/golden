d29960475dd26aae25cbc2b0c1655b4613e1392a
Yuliani Espitia <yuliani.espitia@e-capture.co>
2022-09-20 22:28:18 -0500

    Proyecto controler login actualizado

--- golden/src/main/java/goldenlions/golden/Controllers/LoginControllers.java
+++ golden/src/main/java/goldenlions/golden/Controllers/LoginControllers.java
@@ -1,8 +1,8 @@
 package goldenlions.golden.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+/*import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;*/
 
 @Controller
 public class LoginControllers {
